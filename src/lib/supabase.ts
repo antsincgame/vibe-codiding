163a40326ad09485c2c643e4d87bb9dda1131054
antsinc <121346860+antsincgame@users.noreply.github.com>
2025-12-14 10:34:33 +0300

    Updated .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zxywgueplrosvdwgpmvb.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4eXdndWVwbHJvc3Zkd2dwbXZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzE4NDcsImV4cCI6MjA3ODcwNzg0N30.xyJkFplsbLrBIWEHNksO681xR9htWpV0N45keuc-Uro'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vagtcecbikwvbzacfwqm.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZ3RjZWNiaWt3dmJ6YWNmd3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1NTkwMTksImV4cCI6MjA4MTEzNTAxOX0.kVBgt2AxgRNytrSx77CAsZho_UMaoMJDAAWmDH3FCRw'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Supabase URL or Anon Key is missing!')
