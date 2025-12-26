adabc9c66ffcf609890ce8fbfd2e865028e24faf
antsinc <121346860+antsincgame@users.noreply.github.com>
2025-12-26 16:27:43 +0300

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  },
+  server: {
+    port: 5173
+  }
 });
